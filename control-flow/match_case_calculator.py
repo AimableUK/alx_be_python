b4f4ed80f2f1151e7e967b93e2737e40b5f19057
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-15 09:55:50 +0200

    Incurred using match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,24 +1,22 @@
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the second number:"))
-operation = input("Choose the operation (+, -, *, /):")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
 
-result = 0
-if operation == "+":
-    result = num1 + num2
-
-elif operation == "-":
-    result = num1 - num2
-
-elif operation == "*":
-    result = num1 * num2
-
-elif operation == "/":
-    try:
-        result = num1 / num2
-    except ZeroDivisionError:
-        print("Cannot divide by zero.")
-
-else:
-    result = "Operation Invalid"
-
-print(f"The result is {result}")
+match operation:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
+    case _:
+        print("Invalid operation.")
