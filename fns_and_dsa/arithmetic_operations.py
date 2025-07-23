cdce6a9803638868fabd60d426e0020d3362c293
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-23 09:36:13 +0200

    Changed Zero Division Logic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        try:
-            return num1 / num2
-        except ZeroDivisionError:
+        if num2 == 0:
             return "Can't Divide with Zero"
     else:
         return "Invalid Operation"
