7def58de87076cc6a9e436ccab840969f3195760
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-24 07:12:51 +0200

    Added More Complexity for Value Checking

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,23 +1,39 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+
+def is_number(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
 
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
 
-    print(f'{fahrenheit}°F is {(fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR}°C')
+    if is_number(fahrenheit):
+        fahrenheit = float(fahrenheit)
+        celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+        print(f'{fahrenheit}°F is {celsius}°C')
+    else:
+        print("Invalid temperature. Please enter a numeric value.")
 
 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
 
-    print(f'{celsius}°C is {celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32}°F')
+    if is_number(celsius):
+        celsius = float(celsius)
+        fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+        print(f'{celsius}°C is {fahrenheit}°F')
+    else:
+        print("Invalid temperature. Please enter a numeric value.")
 
 
-temperature = int(input("Enter the temperature to convert: "))
+temperature = input("Enter the temperature to convert: ")
 factor = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
-
-# if temperature / 2 == :
 
 if factor == "F":
     convert_to_celsius(temperature)
